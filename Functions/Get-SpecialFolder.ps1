b5b35ac13ff829fe8fe4c00330f664190a126f20
Øyvind Kallstad <kallstad@gmail.com>
2016-09-02 09:13:11 +0200

    Added default value for parameter

--- Functions/Get-SpecialFolder.ps1
+++ Functions/Get-SpecialFolder.ps1
@@ -1,5 +1,5 @@
 function Get-SpecialFolder {
-    param ([string]$Name)
+    param ([string]$Name = '*')
     foreach ($folder in (([Enum]::GetValues([System.Environment+SpecialFolder])) | Where-Object {$_ -like $Name})) {
         Write-Output (,([PSCustomObject] @{
             Name = $folder.ToString()
